ea3c26359d45edefb659120b3d4c0411fe2fbfc5
Dev Kiddz <denngodfirst@gmail.com>
2026-05-13 10:42:39 +0100

    Create initial dashboard layout and UI structure

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,16 @@
-import { Moon } from 'lucide-react';
+import { Group, LogOut, Moon, Settings, User } from 'lucide-react';
 import Link from 'next/link';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
 
 export default function Navbar() {
   return (
@@ -12,10 +22,39 @@ export default function Navbar() {
         <Link href="/">Dashboard</Link>
         <Moon />
 
-        <Avatar>
-          <AvatarImage src="https://avatars.githubusercontent.com/u/56824782?v=4" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+        {/* User's drop down options menu with avatar */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar className="cursor-pointer">
+              <AvatarImage src="https://avatars.githubusercontent.com/u/56824782?v=4" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10}>
+            <DropdownMenuGroup>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuItem>
+                <User className="w-[1.2rem] h-[1.2rem] mr-2" />
+                Profile
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Group className="w-[1.2rem] h-[1.2rem] mr-2" />
+                Membership
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem variant="destructive">
+                <Settings className="w-[1.2rem] h-[1.2rem] mr-2" />
+                Settings
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <LogOut className="w-[1.2rem] h-[1.2rem] mr-2" />
+                Logout!
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );
