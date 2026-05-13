4af0740e715d57a025f8c325b0c9b95871911de3
Dev Kiddz <denngodfirst@gmail.com>
2026-05-13 21:23:56 +0100

    Adding the Revenue Bar Chart. ( Engine #Rechartsc )

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
+import AppBarchart from '@/components/AppBarChart';
 import { Maname } from 'next/font/google';
 import Image from 'next/image';
 
 export default function Home() {
-  return <div className="flex">Hello, Next.js!</div>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarchart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Hello, Next.js!</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Hello, Next.js!</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Hello, Next.js!</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        Hello, Next.js!
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Hello, Next.js!</div>
+    </div>
+  );
 }
