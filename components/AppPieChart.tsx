f39b414236f455effb33d04f55a81620774e8a57
Dev Kiddz <denngodfirst@gmail.com>
2026-05-14 06:39:44 +0100

    Integrate transaction data display and dashboard analytics

--- components/AppPieChart.tsx
+++ components/AppPieChart.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from './ui/chart';
 
 import { PieChart, Pie, Label } from 'recharts';
+import { TrendingUp } from 'lucide-react';
 
 const chartConfig = {
   visitors: {
@@ -67,6 +68,12 @@ export default function AppPieChart() {
           </Pie>
         </PieChart>
       </ChartContainer>
+      <div className="mt-4 flex flex-col gap-2 items-center">
+        <div className="flex items-center gap-2 leading-none font-medium">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4 text-[#02BB7C]" />
+        </div>
+        <div className="leading-none text-muted-foreground">Showing total visitors for the last 6 months</div>
+      </div>
     </div>
   );
 }
