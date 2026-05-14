fea4f0b4a76d4e0fa66509384adad2484ef7a17d
Dev Kiddz <denngodfirst@gmail.com>
2026-05-14 17:33:06 +0100

    Fixed the table problems

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -73,11 +73,11 @@ function Calendar({
           defaultClassNames.caption_label
         ),
         // table: "w-full border-collapse",
-        weekdays: cn('flex', defaultClassNames.weekdays),
-        weekday: cn(
-          'flex-1 rounded-(--cell-radius) text-[0.8rem] font-normal text-muted-foreground select-none',
-          defaultClassNames.weekday
-        ),
+        // weekdays: cn('flex', defaultClassNames.weekdays),
+        // weekday: cn(
+        //   'flex-1 rounded-(--cell-radius) text-[0.8rem] font-normal text-muted-foreground select-none',
+        //   defaultClassNames.weekday
+        // ),
         week: cn('mt-2 flex w-full', defaultClassNames.week),
         week_number_header: cn('w-(--cell-size) select-none', defaultClassNames.week_number_header),
         week_number: cn('text-[0.8rem] text-muted-foreground select-none', defaultClassNames.week_number),
